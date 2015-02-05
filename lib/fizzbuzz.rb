6ecaf6422a7c526a6cf78498ed2861ded74d5030
Erik Griffin <griffin.erik@gmail.com>
2015-02-05 14:06:45 +0000

    Completed fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,26 @@
 class FizzBuzz
 
+  def divides_by_three?(number)
+    number % 3 == 0
 
+  end
+
+  def divides_by_five?(number)
+    number % 5 == 0
+  end
+
+  def divides_by_fifteen?(number)
+    divides_by_three?(number) && divides_by_five?(number)
+  end
+
+  def query(number)
+
+    return 'fizzbuzz' if divides_by_fifteen?(number)
+    return 'fizz' if divides_by_three?(number)
+    return 'buzz' if divides_by_five?(number)
+    number
+
+  end
 
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,47 @@ require 'FizzBuzz'
 
 describe FizzBuzz do
 
+  let (:fizzbuzz) {FizzBuzz.new}
 
+
+  it 'should know 1 is not divisible by 3' do
+    expect(fizzbuzz.divides_by_three?(1)).to eq false
+  end
+
+  it 'should know 3 is divisible by 3' do
+    expect(fizzbuzz.divides_by_three?(3)).to eq true
+  end
+
+  it 'should know 1 is not divisible by 5' do
+    expect(fizzbuzz.divides_by_five?(1)).to eq false
+  end
+
+  it 'should know 5 is divisible by 5' do
+    expect(fizzbuzz.divides_by_five?(5)).to eq true
+  end
+
+  it 'should know 1 is not divisible by 3 AND 5' do
+    expect(fizzbuzz.divides_by_fifteen?(1)).to eq false
+  end
+
+  it 'should know 15 is divisible by 3 AND 5' do
+    expect(fizzbuzz.divides_by_fifteen?(15)).to eq true
+  end
+
+  it 'should print 1 when asked 1' do
+    expect(fizzbuzz.query(1)).to eq 1
+  end
+
+  it 'should print "fizz" when asked 3' do
+    expect(fizzbuzz.query(3)).to eq 'fizz'
+  end
+
+  it 'should print "buzz" when asked 5' do
+    expect(fizzbuzz.query(5)).to eq 'buzz'
+  end
+
+  it 'should print "fizzbuzz" when asked 15' do
+    expect(fizzbuzz.query(15)).to eq 'fizzbuzz'
+  end
 
 end
